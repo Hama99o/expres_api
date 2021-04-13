9ecd1c1c3a5531627ad6cf658747bbc3d28c81ce
Hama <hmms@gmail.com>
2021-04-13 17:21:29 +0200

    new update

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const = express = require('express')
+const router = express.Router
+
+
+const users = [{name: 'hama', email: 'hmmshl@gmail.com'}]
+
+router.get ('/', (_, res) => {
+  res.send('your Express router')
+})
+
+router.get ('/users', (_, res) => {
+  res.json({ ok: true, users })
+})
+
+router.get('/user/:name', (req, res) => {
+  const { name } = req.params
+  const user = users.filter((user) => user.name === name)[0]
+  res.json({  ok: true,  user })
+})
+router.post ('/adduser',(req, res) => {
+  const { name, email } = req.body
+  if (name && email ){
+    users.push({name, email })
+    res.json({ok: true, users })
+  }
+})
